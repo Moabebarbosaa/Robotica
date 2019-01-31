374d133092715d71c30eadd470e0c978d2fe5d92
Moabe Barbosa Alves <Moabebarbosaa@gmail.com>
2019-01-31 16:50:19 -0300

    Update Robozin.py
    Estamos verificando as cores e virando para esquerda, direita, frente.

--- Robozin.py
+++ Robozin.py
@@ -4,18 +4,53 @@ from ev3dev.ev3 import *
 motorA = LargeMotor('outA')
 motorB = LargeMotor('outB')
 
-# motorA.run_forever(speed_sp=200)
-# motorB.run_forever(speed_sp=200)
-
-# sensor = UltrasonicSensor('in4')
-
-# giroscopio = GyroSensor('in1')
-# giroscopio.mode = 'GYRO-ANG'
-# unidades = giroscopio.units
-
-# units = sensor.units
-
 sensorInfra = InfraredSensor("in1")
+sensorCor = ColorSensor("in2")
+MODE_COL_COLOR = 'COL-COLOR'
+
+branco, preto, azul, verde, vermelho = 6, 1, 2, 3, 5
+corLida = sensorCor.color
+
+
+def virarDireita():
+    motorA = LargeMotor('outA')
+    motorB = LargeMotor('outB')
+
+    for i in range(700):
+        motorA.run_forever(speed_sp=200)
+        motorB.run_forever(speed_sp=200)
+
+    for i in range(500):
+        motorA.run_forever(speed_sp=200)
+        motorB.run_forever(speed_sp=-200)
+
+    for i in range(500):
+        motorA.run_forever(speed_sp=200)
+        motorB.run_forever(speed_sp=200)
+
+def seguirFrente():
+    motorA = LargeMotor('outA')
+    motorB = LargeMotor('outB')
+
+    for i in range(1500):
+        motorA.run_forever(speed_sp=200)
+        motorB.run_forever(speed_sp=200)
+
+def virarEsquerda():
+    motorA = LargeMotor('outA')
+    motorB = LargeMotor('outB')
+
+    for i in range(700):
+        motorA.run_forever(speed_sp=200)
+        motorB.run_forever(speed_sp=200)
+
+    for i in range(500):
+        motorA.run_forever(speed_sp=-200)
+        motorB.run_forever(speed_sp=200)
+
+    for i in range(500):
+        motorA.run_forever(speed_sp=200)
+        motorB.run_forever(speed_sp=200)
 
 
 def funcao_saturacao(v):
@@ -26,43 +61,28 @@ def funcao_saturacao(v):
     else:
         return v
 
-
 offset = 28
-constProp = 50
+constProp = 30
 
 try:
     while True:
         erro = offset - sensorInfra.value()
         giro = erro * constProp
 
+        corLida = sensorCor.color
+
         motorA.run_forever(speed_sp=funcao_saturacao(170 - giro))
         motorB.run_forever(speed_sp=funcao_saturacao(170 + giro))
+
+        if str(corLida) == str(vermelho):
+            seguirFrente()
+        if str(corLida) == str(verde):
+            virarDireita()
+        if str(corLida) == str(azul):
+            virarEsquerda()
+
+
+        print("valor = " + str(corLida))
 except KeyboardInterrupt:
     motorA.stop()
     motorB.stop()
-
-# verificar quando faz a curva perto da borda (esta caindo). precisa controlar isso!!
-
-
-# while(True):
-#
-#     distancia = sensor.value() / 10
-#     angle = giroscopio.value()
-#
-#     if distancia < 20:
-#         giro = 0
-#         ang_anterior = giroscopio.value()
-#         while giro < 90:
-#             giro = ang_anterior - giroscopio.value()
-#             print(giro, ang_anterior, giroscopio.value())
-#             motorA.run_forever(speed_sp=-200)
-#             motorB.run_forever(speed_sp=200)
-#
-#
-#     else:
-#         motorA.run_forever(speed_sp=200)
-#         motorB.run_forever(speed_sp=200)
-#
-#
-#     print(str(angle))
-#     print(str(distancia) + " " + units)
