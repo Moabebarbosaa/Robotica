f9c1a5b3af03a3b0f2e73ad6170a3dc1ab7725a9
Moabe Barbosa Alves <Moabebarbosaa@gmail.com>
2019-02-06 11:39:11 -0300

    Update Robozin.py
    Ta quase aprendendo as cores. Falta alguns ajustes.
    Prox passo: Se aprender duas cores, por exclusão aprender a terceira.

--- Robozin.py
+++ Robozin.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 from ev3dev.ev3 import *
 
-def moda(l):
-    repeticoes = 0
-    valor = 0
-    for i in range(len(l)):
-        aparicoes = l.count(l[i])
-        if aparicoes > repeticoes:
-            repeticoes = aparicoes
-            valor = l[i]
 
-    return valor
+def funcao_saturacao(v):
+    if v > 1000:
+        return 1000
+    elif v < -1000:
+        return -1000
+    else:
+        return v
+
+def andar():
+    offset = 28
+    constProp = 30
+
+    erro = offset - sensorInfra.value()
+    giro = erro * constProp
+
+    motorA.run_forever(speed_sp=funcao_saturacao(170 - giro))
+    motorB.run_forever(speed_sp=funcao_saturacao(170 + giro))
+
+
 
 def virarDireita(motorA, motorB):
     for i in range(700):
@@ -22,8 +32,8 @@ def virarDireita(motorA, motorB):
         motorB.run_forever(speed_sp=-200)
 
     for i in range(500):
-        motorA.run_forever(speed_sp=200)
-        motorB.run_forever(speed_sp=200)
+        motorA.run_forever(speed_sp=300)
+        motorB.run_forever(speed_sp=300)
 
 def seguirFrente(motorA, motorB):
     for i in range(1500):
@@ -43,109 +53,109 @@ def virarEsquerda(motorA, motorB):
         motorA.run_forever(speed_sp=300)
         motorB.run_forever(speed_sp=300)
 
-def saberCor():
 
-    return 0
 
-def funcao_saturacao(v):
-    if v > 1000:
-        return 1000
-    elif v < -1000:
-        return -1000
-    else:
-        return v
+def saberGiro(contEsquerda):
+    if contEsquerda == 1:
+        return "Esquerda"
+    elif contEsquerda == 2:
+        return "Seguir"
+    elif contEsquerda == 3:
+        return "Direita"
 
-def addListaCor(cont):
-    listaCor.append(corLida)
-    print(listaCor)
-    cont += 1
 
-def setaDicionarioCores(dicioCores,qtdQueViraEsquerda,primeiraCor):
 
-    if qtdQueViraEsquerda == 0:
-        dicioCores[primeiraCor] = "E"
-    elif qtdQueViraEsquerda == 1:
-        dicioCores[primeiraCor] = "F"
-    elif qtdQueViraEsquerda == 2:
-        dicioCores[primeiraCor] = "D"
-    return dicioCores
+def SaberLado(cor):
 
-def contaPreto(qtdViraEsquerda):
-    qtdViraEsquerda += 1
-    print("CONTEI UM PRETO")
-    return qtdQueViraEsquerda
+    contCor = 0
+    contCorTotal = 0
 
-def condicoesAPartirDicionario(dicioCores,listaCor):
-    if dicioCores[moda(listaCor)] == "E":
-        virarEsquerda(motorA, motorB)
-    elif dicioCores[moda(listaCor)] == "D":
-        virarDireita(motorA, motorB)
-    elif dicioCores[moda(listaCor)] == "F":
+    coresIndesejadas = [7, 6, 1, 0, 4]
+
+    while True:
+        corLida = sensorCor.color
+        andar()
+
+        while (corLida == cor):
+            andar()
+            corLida = sensorCor.color
+            contCor += 1
+
+        if contCor > 1:
+            contCorTotal += 1
+            contCor = 0
+            print("ContCorTotal: ", contCorTotal)
+
+        if (corLida != cor and corLida not in coresIndesejadas) or contCorTotal == 3:
+            print("ENTROU AQUI: ", contCorTotal)
+            print(corLida)
+            break
+
+    return saberGiro(contCorTotal)
+
+
+
+def Acao(acao, motorA, motorB):
+    if acao == "Seguir":
         seguirFrente(motorA, motorB)
-    return dicioCores
+    elif acao == "Direita":
+        virarDireita(motorA, motorB)
+    elif acao == "Esquerda":
+        virarEsquerda(motorA, motorB)
+
+
+
 
 motorA = LargeMotor('outA')
 motorB = LargeMotor('outB')
 
 sensorInfra = InfraredSensor("in1")
+
 sensorCor = ColorSensor("in2")
 MODE_COL_COLOR = 'COL-COLOR'
 
 semCor, preto, azul, verde, vermelho, branco = 0, 1, 2, 3, 5, 6
-CoresValidas = []
-dicioCores = {0: "", 1: "", 2: "", 3: "", 5: "", 6: ""}
 
-corVermelha = "Esquerda"
-corVerde = "Direita"
-corAzul = "Seguir"
+corVermelha = ""
+corVerde = ""
+corAzul = ""
 
-offset = 35
-constProp = 30
-
-qtdQueViraEsquerda = 0
-achouACor = False
-
-listaCor = [-1]
-cont = 0
 try:
     while True:
-        erro = offset - sensorInfra.value()
-        giro = erro * constProp
 
         corLida = sensorCor.color
-        motorA.run_forever(speed_sp=funcao_saturacao(170 - giro))
-        motorB.run_forever(speed_sp=funcao_saturacao(170 + giro))
 
-        print("MODA: ", moda(listaCor))
-#       print(sensorCor.color)
+        #print("Cor Lida: ", corLida)
 
-        # Tratamento de transição de cores
+        if (corLida == branco or corLida == preto):
+            andar()
 
-        # if corLida == semCor:
-        #     re(motorA,motorB)
-        print(cont)
-        if cont < 10 and corLida != 6:
-            listaCor.append(corLida)
-            cont +=1
 
-        elif corLida == 6:
-            listaCor = [-1]
-            cont = 0
+        if (corLida == azul):
+            print("AZUL")
+            if corAzul == "":
+                corAzul = SaberLado(azul)
+            else:
+                Acao(corAzul, motorA, motorB)
 
-        elif cont > 9:
-            print ("=============ENTREI================= ")
-            primeiraCor = moda(listaCor)
 
-            if moda(listaCor) == preto:
-                qtdQueViraEsquerda = contaPreto(qtdQueViraEsquerda)
+        if (corLida == verde):
+            print("VERDE")
+            if corVerde == "":
+                print("AQUI")
+                corVerde = SaberLado(verde)
+            else:
+                Acao(corVerde, motorA, motorB)
 
-            if moda(listaCor) != primeiraCor and moda(listaCor) != preto and moda(listaCor) != branco and moda(
-                    listaCor) != semCor:
-                setaDicionarioCores(dicioCores, qtdQueViraEsquerda,primeiraCor)
 
-            print(dicioCores)
-            # condicoesAPartirDicionario(dicioCores,listaCor)
-            # print(setaDicionarioCores(dicioCores, qtdQueViraEsquerda,primeiraCor))
+        if (corLida == vermelho):
+            print("VERMELHO")
+            if corVermelha == "":
+                corVermelha = SaberLado(vermelho)
+            else:
+                Acao(corVermelha, motorA, motorB)
+
+
 
 
 except KeyboardInterrupt:
