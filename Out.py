47b75d6b295b81101e371f4f1c1ee9231a3bf7a6
João Carlos Guerra <36782577+JoaocGuerra@users.noreply.github.com>
2019-03-13 15:15:15 -0300

    Update Out.py
    Organização do código.

--- Out.py
+++ Out.py
@@ -1,51 +1,29 @@
 def sair_Quadrado():
-    posicao_motor_D = motorDireito.position
-    posicao_motor_E = motorEsquerdo.position
+    cont = 0
+    while cont <= 4:
+        posicao_motor_D = motorDireito.position
+        posicao_motor_E = motorEsquerdo.position
+        if cont == 0 or cont == 2 or cont == 4:
+            qtd = 700
+        elif cont == 1:
+            qtd = 1000
+        else:
+            qtd = 500
 
-    for i in range(700):
-        motorDireito.run_to_abs_pos(position_sp=posicao_motor_D-600, speed_sp=100)
-        motorEsquerdo.run_to_abs_pos(position_sp=posicao_motor_E+600, speed_sp=100)
-    print("Rodou")
-    posicao_motor_D = motorDireito.position
-    posicao_motor_E = motorEsquerdo.position
-
-    for i in range(1000):
-        motorEsquerdo.run_forever(speed_sp=300)
-        motorDireito.run_forever(speed_sp=300)
-
-    print("Frente")
-    posicao_motor_D = motorDireito.position
-    posicao_motor_E = motorEsquerdo.position
-
-    for i in range(550):
-        motorDireito.run_to_abs_pos(position_sp=posicao_motor_D-600, speed_sp=100)
-        motorEsquerdo.run_to_abs_pos(position_sp=posicao_motor_E+600, speed_sp=100)
-
-    print("Rodou")
-    posicao_motor_D = motorDireito.position
-    posicao_motor_E = motorEsquerdo.position
-
-    for i in range(500):
-        andarSensoresquerdo()
-
-    print("sensor")
-    posicao_motor_D = motorDireito.position
-    posicao_motor_E = motorEsquerdo.position
-
-    for i in range(550):
-        motorDireito.run_to_abs_pos(position_sp=posicao_motor_D - 600, speed_sp=100)
-        motorEsquerdo.run_to_abs_pos(position_sp=posicao_motor_E + 600, speed_sp=100)
-
-    print("Rodou")
-    posicao_motor_D = motorDireito.position
-    posicao_motor_E = motorEsquerdo.position
+        for i in range(qtd):
+            if cont == 0 or cont == 2 or cont == 4:
+                motorDireito.run_to_abs_pos(position_sp=posicao_motor_D - 600, speed_sp=100)
+                motorEsquerdo.run_to_abs_pos(position_sp=posicao_motor_E + 600, speed_sp=100)
+            elif cont == 1:
+                motorEsquerdo.run_forever(speed_sp=300)
+                motorDireito.run_forever(speed_sp=300)
+            else:
+                andarSensoresquerdo()
+        cont += 1
 
     while True:
         andarSensoresquerdo()
 
-    print("Sensor")
-    posicao_motor_D = motorDireito.position
-    posicao_motor_E = motorEsquerdo.position
 
 
 def funcao_saturacao(v):
@@ -55,6 +33,7 @@ def funcao_saturacao(v):
         return -1000
     else:
         return v
+
 
 def andarSensoresquerdo():
     offset = 4
@@ -71,4 +50,3 @@ def andarSensoresquerdo():
     elif erro < 4:
         motorEsquerdo.run_forever(speed_sp=funcao_saturacao(200 + giro))
         motorDireito.run_forever(speed_sp=funcao_saturacao(200 - giro))
-
