3466b4c17b9da933ee11074c5075cece48ed04a4
Lucas Matheus <34188223+lucasmatheus1@users.noreply.github.com>
2019-03-18 12:51:19 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -2,40 +2,67 @@
 
 import paho.mqtt.client as mqtt
 from ev3dev.ev3 import *
+# import Queue
+from sys import excepthook
 
-motor = LargeMotor("outA")
+ultra = False
+MOTOR = LargeMotor("outA")
+
+client = mqtt.Client()
+
+client.connect("10.42.0.243", 1883, 60)
+
+
+def exception_handle(exctype, value, tb):
+    print("Type:", exctype)
+    print("Value:", value)
+    print("Traceback:", tb)
+
+excepthook = exception_handle
+
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-    client.subscribe("topic/test")
+    client.subscribe([("topic/teste", 0)])
+
 
 def on_disconnect(client, userdata, rc=0):
     client.loop_stop()
 
-def printalegal():
-    print ("legal")
+
+def on_message(client, userdata, msg):
+    global ultra
+
+    if msg.topic == "topic/teste":
+
+        # print(ultra)
+        ultra = float(msg.payload)
+        # print (ultra)
 
 def capturaSensor(valor):
     if int(valor) < 60:
-        motor.run_forever(speed_sp=200)
-    elif int (valor) >= 60:
-        motor.stop()
+        MOTOR.run_forever(speed_sp=200)
+    elif int(valor) >= 60:
+        MOTOR.stop()
+
+def andar():
+    global ultra
+    # capturaSensor(ultra)
+    # if ultra == True:
+    #     print ("legal")
+
+    print (ultra)
+
+def main():
+
+        # print(ultra)
+        client.on_connect = on_connect
+        client.on_message = on_message
+        client.loop_start()
+
+        while True:
+            andar()
 
 
-def on_message(client, userdata, msg):
-    # global test
-    # teste = str(msg.payload.decode())
-    print (msg.payload.decode())
-    capturaSensor(msg.payload)
 
-    printalegal()
-
-
-
-client = mqtt.Client()
-client.connect("169.254.154.65",1883,60)
-
-client.on_connect = on_connect
-client.on_message = on_message
-
-client.loop_forever()
+if __name__ == '__main__':
+    main()
