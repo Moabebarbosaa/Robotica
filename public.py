b8b7675432974faf2cae6ab77c675fec9c756e45
Lucas Matheus <34188223+lucasmatheus1@users.noreply.github.com>
2019-03-18 12:51:58 -0300

    Update public.py

--- public.py
+++ public.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
+from ev3dev.ev3 import UltrasonicSensor, ColorSensor, Button
+from os import system
 import paho.mqtt.client as mqtt
-from ev3dev.ev3 import *
 
-ultra = UltrasonicSensor("in1")
+# Sensores ultrasonicos
+ULTRA1 = UltrasonicSensor("in1")
 
-#  This is the Publisher
+client = mqtt.Client()
+client.connect("localhost", 1883, 60)
+client.loop_start()
+
+DISTANCIA_BONECOS = 20
+
+
+def on_disconnect(client, userdata, rc=0):
+    client.loop_stop()
+
+
+client.on_disconnect = on_disconnect
 
 while True:
-        client = mqtt.Client()
-        client.connect("localhost",1883,60)
-        client.publish(topic="topic/test", payload=ultra.value(),qos=1,retain=False)
+    distancia1 = ULTRA1.value() / 10
+
+    if distancia1 <= DISTANCIA_BONECOS:
+        client.publish(topic="topic/teste", payload=distancia1, qos=0, retain=False)
