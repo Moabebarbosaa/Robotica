c4ae3309de93005a08eaf9e8a6b697be61bcd4e9
Moabe Barbosa Alves <Moabebarbosaa@gmail.com>
2019-04-02 12:17:29 -0300

    Update 2-Servidor.py

--- 2-Servidor.py
+++ 2-Servidor.py
@@ -1,35 +1,24 @@
 #!/usr/bin/env python3
 
-from ev3dev.ev3 import ColorSensor
+from ev3dev.ev3 import *
 import paho.mqtt.client as mqtt
 
 
-sensorCorEsquerdo = ColorSensor("in1")
-sensorCorDireito = ColorSensor("in2")
-sensorCorEsquerdo.mode = 'COL-COLOR'
-sensorCorDireito.mode = 'COL-COLOR'
-
+ultrassonico = UltrasonicSensor('in1')
 
 client = mqtt.Client()
 client.connect("localhost", 1883, 60)
 
-cor_anterior1 = ""
-cor_anterior2 = ""
 
 def on_disconnect(client, userdata, rc=0):
     client.loop_stop()
 
 client.on_disconnect = on_disconnect
 
-
+distanciaPermitida = 20
 
 while True:
-    cor_atual1 = sensorCorEsquerdo.value()
-    cor_atual2 = sensorCorDireito.value()
+    ultrassonicoAtual = ultrassonico.value() / 10
 
-    if cor_atual1 != cor_anterior1:
-        client.publish(topic="topic/sensor/color1", payload=cor_atual1, qos=0, retain=False)
-        cor_anterior1 = cor_atual1
-    if cor_atual2 != cor_anterior2:
-        client.publish(topic="topic/sensor/color2", payload=cor_atual2, qos=0, retain=False)
-        cor_anterior2 = cor_atual2
+    if ultrassonicoAtual <= distanciaPermitida:
+        client.publish(topic="topic/sensor/ultra", payload=True, qos=0, retain=False)
