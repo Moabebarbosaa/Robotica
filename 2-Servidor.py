32835c4caf4dfd81f32813a973d9a14b18a77245
João Carlos Guerra <36782577+JoaocGuerra@users.noreply.github.com>
2019-04-10 20:20:29 -0300

    Update 2-Servidor.py

--- 2-Servidor.py
+++ 2-Servidor.py
@@ -2,6 +2,7 @@
 
 from ev3dev.ev3 import *
 import paho.mqtt.client as mqtt
+from os import system
 
 
 ultrassonico = UltrasonicSensor('in1')
@@ -16,10 +17,22 @@ def on_disconnect(client, userdata, rc=0):
 client.on_disconnect = on_disconnect
 
 distanciaPermitida = 20
+botao = Button()
 
+# print("\n\n\n\n\n\n\n\n\n\n   ----- Botao do meio para iniciar -----")
+# while True:
+#     if botao.enter:
+#         system("clear")
+#         break
 
 while True:
     ultrassonicoAtual = ultrassonico.value() / 10
 
+    # if botao.enter:
+    #     print("\n\n\n\n\n\n\n\n\n\n   ----- Iniciou novamente -----")
+    #     system("clear")
+
+
     if ultrassonicoAtual <= distanciaPermitida:
         client.publish(topic="topic/sensor/ultra", payload=True, qos=0, retain=False)
+        print(12345)
